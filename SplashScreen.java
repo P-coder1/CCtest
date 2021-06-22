418989bc8c40ab1f999f19059434d57062ed2c2e
P-coder1 <48465064+P-coder1@users.noreply.github.com>
2021-06-22 17:35:14 +0900

    Add files via upload
    로딩화면

---
+++ SplashScreen.java
@@ -0,0 +1,38 @@
+package com.obliging.story;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+//로딩화면 만드는 소스
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        startLoading();
+
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
